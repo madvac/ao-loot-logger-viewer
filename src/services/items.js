f938c08333e2c27af4ec59511a3158ebb061aacf
Matheus Sampaio <matheus@sampaio.us>
2021-06-26 03:12:25 -0500

    refactor: items service should be more stable

--- src/services/items.js
+++ src/services/items.js
@@ -2,42 +2,46 @@ import axios from 'axios'
 
 class Items {
   constructor() {
-    this.idToName = {}
-    this.locNameVarToName = {}
-    this.indexToName = {}
-    this.nameToInfo = {}
+    this.items = {}
+
+    // this.idToName = {}
+    // this.locNameVarToName = {}
+    // this.indexToName = {}
+    // this.nameToInfo = {}
 
     this.isInitialized = false
   }
 
   getNameFromId(itemId) {
-    return this.idToName[itemId] || this.locNameVarToName[`@ITEMS_${itemId}`]
+    return this.items[itemId]?.name || this.items[`@ITEMS_${itemId}`]?.name
   }
 
   getNameFromIndex(itemIndex) {
-    return this.indexToName[itemIndex]
+    return this.items[itemIndex].name
   }
 
   getIdFromName(itemName) {
-    return this.nameToInfo[itemName].id
+    return this.items[itemName].id
   }
 
   getInfoFromName(itemName) {
-    return this.nameToInfo[itemName]
+    return this.items[itemName]
   }
 
   getInfoFromId(itemId) {
-    return this.getInfoFromName(this.getNameFromId(itemId))
+    return this.items[itemId]
   }
 
   getInfoFromIndex(itemIndex) {
-    return this.getInfoFromName(this.getNameFromIndex(itemIndex))
+    return this.items[itemIndex]
   }
 
   async init(sha = null) {
     if (sha === this.sha && this.isInitialized) {
       return
     }
+
+    console.time('items.init')
 
     this.isInitialized = true
 
@@ -56,19 +60,36 @@ class Items {
     )
 
     for (const item of response.data) {
-      const name = item.LocalizedNames && item.LocalizedNames['EN-US'] ? item.LocalizedNames['EN-US'] : item.UniqueName
+      const locName = item.LocalizedNames && item.LocalizedNames['EN-US'] ? item.LocalizedNames['EN-US'] : null
+      const name = locName || item.UniqueName
 
-      this.idToName[item.UniqueName] = name
-      this.locNameVarToName[item.LocalizationNameVariable] = name
-      this.indexToName[item.Index] = name
-
-      if (this.nameToInfo[name] == null || item.UniqueName.indexOf('@') === -1) {
-        this.nameToInfo[name] = {
-          name,
-          id: item.UniqueName,
-          index: item.Index
-        }
+      const data = {
+        name,
+        locName,
+        locNameVar: item.LocalizationNameVariable,
+        id: item.UniqueName,
+        index: item.Index
       }
+
+      this.items[item.UniqueName] = data
+      this.items[item.LocalizationNameVariable] = data
+      this.items[item.Index] = data
+
+      if (this.items[name] == null || item.UniqueName.indexOf('@') === -1) {
+        this.items[name] = data
+      }
+
+      // this.idToName[item.UniqueName] = name
+      // this.locNameVarToName[item.LocalizationNameVariable] = name
+      // this.indexToName[item.Index] = name
+
+      // if (this.nameToInfo[name] == null || item.UniqueName.indexOf('@') === -1) {
+      //   this.nameToInfo[name] = {
+      //     name,
+      //     id: item.UniqueName,
+      //     index: item.Index
+      //   }
+      // }
     }
 
     response = await axios.get(`https://raw.githubusercontent.com/broderickhyman/ao-bin-dumps/${this.sha}/items.json`)
@@ -77,8 +98,8 @@ class Items {
       const id = item['@uniquename']
       const name = this.getNameFromId(id)
 
-      this.nameToInfo[name].category = 'skin'
-      this.nameToInfo[name].subcategory = 'mountskin'
+      this.items[name].category = 'skin'
+      this.items[name].subcategory = 'mountskin'
     }
 
     const ignoreGroups = {
@@ -100,50 +121,51 @@ class Items {
       }
 
       for (const item of group) {
-        const id = item['@uniquename']
-        const name = this.getNameFromId(id)
+        for (let i = 0; i <= 3; i++) {
+          const id = i ? `${item['@uniquename']}@${i}` : item['@uniquename']
 
-        if (this.nameToInfo[name] == null) {
-          console.error('Invalid item name', groupName, item)
+          if (this.items[id] == null) {
+            continue
+          }
 
-          continue
-        }
+          this.items[id].tier = parseInt(item['@tier'], 10)
+          this.items[id].category = item['@shopcategory']
+          this.items[id].subcategory = item['@shopsubcategory1']
 
-        this.nameToInfo[name].tier = parseInt(item['@tier'], 10)
-        this.nameToInfo[name].category = item['@shopcategory']
-        this.nameToInfo[name].subcategory = item['@shopsubcategory1']
+          if (item['@slottype']) {
+            this.items[id].slotType = item['@slottype']
+          }
 
-        if (item['@slottype']) {
-          this.nameToInfo[name].slotType = item['@slottype']
-        }
+          if (this.items[id].tier == null) {
+            console.warn('Item without Tier', groupName, item)
+          }
 
-        if (this.nameToInfo[name].tier == null) {
-          console.warn('Item without Tier', groupName, item)
-        }
+          if (this.items[id].category == null) {
+            console.warn('Item without Category', groupName, item)
+          }
 
-        if (this.nameToInfo[name].category == null) {
-          console.warn('Item without Category', groupName, item)
-        }
+          if (this.items[id].subcategory == null) {
+            console.warn('Item without SubCategory', groupName, item)
+          }
 
-        if (this.nameToInfo[name].subcategory == null) {
-          console.warn('Item without SubCategory', groupName, item)
-        }
+          if (this.items[id].category === 'products' && this.items[id].subcategory === 'journal') {
+            const journalFull = this.getNameFromId(`${id}_FULL`)
 
-        if (this.nameToInfo[name].category === 'products' && this.nameToInfo[name].subcategory === 'journal') {
-          const journalFull = this.getNameFromId(`${id}_FULL`)
+            this.items[journalFull].tier = parseInt(item['@tier'], 10)
+            this.items[journalFull].category = item['@shopcategory']
+            this.items[journalFull].subcategory = item['@shopsubcategory1']
 
-          this.nameToInfo[journalFull].tier = parseInt(item['@tier'], 10)
-          this.nameToInfo[journalFull].category = item['@shopcategory']
-          this.nameToInfo[journalFull].subcategory = item['@shopsubcategory1']
+            const journalEmpty = this.getNameFromId(`${id}_EMPTY`)
 
-          const journalEmpty = this.getNameFromId(`${id}_EMPTY`)
-
-          this.nameToInfo[journalEmpty].tier = parseInt(item['@tier'], 10)
-          this.nameToInfo[journalEmpty].category = item['@shopcategory']
-          this.nameToInfo[journalEmpty].subcategory = item['@shopsubcategory1']
+            this.items[journalEmpty].tier = parseInt(item['@tier'], 10)
+            this.items[journalEmpty].category = item['@shopcategory']
+            this.items[journalEmpty].subcategory = item['@shopsubcategory1']
+          }
         }
       }
     }
+
+    console.timeEnd('items.init')
   }
 }
 
