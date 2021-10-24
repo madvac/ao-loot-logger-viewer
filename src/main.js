9a6f650e3eae88fe9203d6006b8b93e6d121d5fa
Matheus Sampaio <matheus@sampaio.us>
2021-10-24 15:14:28 -0700

    feat: add router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import iziToast from 'izitoast'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 import App from './App.vue'
 import store from './store'
+import router from './router'
+
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
@@ -21,5 +26,6 @@ iziToast.settings({
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
