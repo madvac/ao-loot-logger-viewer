ee2fcd39372c47026214a5dd56230b1e3fce6ca4
Matheus Sampaio <matheus@sampaio.us>
2022-10-01 13:31:20 -0700

    feat: update loot log regex to relax the lootedFrom username

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,7 +1,7 @@
 const guildMemberLogRe = /^(("\d*"\t"(?<userName1>.*)"\t".*"\t"-?\d*")|(?<userName2>\w+)|(\d*,(?<userName3>.*),.*,-?\d*))$/gm
 const lootLogRe = /^(?<lootedAt>[^;]*);(?<lootedBy>\w+);(?<itemId>[\w@]+);(?<amount>\d*);(?<lootedFrom>[\w@]+?);(?<itemName>.*)$/gm
 const v2LootLogRe =
-  /^(?<lootedAt>[^;]*);(?<lootedByAlliance>.*);(?<lootedByGuild>.*);(?<lootedBy>\w+);(?<itemId>[\w@]+);(?<itemName>.*);(?<amount>\d*);(?<lootedFromAlliance>.*);(?<lootedFromGuild>.*);(?<lootedFrom>[\w@]+)$/gm
+  /^(?<lootedAt>[^;]*);(?<lootedByAlliance>.*);(?<lootedByGuild>.*);(?<lootedBy>\w+);(?<itemId>[\w@]+);(?<itemName>.*);(?<amount>\d*);(?<lootedFromAlliance>.*);(?<lootedFromGuild>.*);(?<lootedFrom>.+)$/gm
 const aoLootLogRe = /^"(?<lootedAt>[^,]*)","(?<lootedBy>\w+?)","(?<guildName>.*)","(?<item>.*)","(?<itemName>.*) \((?<itemId>[\w@]+)\)","(?<amount>\d*)x .+","(?<lootedFrom>[\w@]+?)"/gm
 const albionAnalysisLootLogRe = /^(?<lootedAt>[^;]*);(?<lootedBy>\w+);(?<itemId>[\w@]+);(?<amount>\d*);(?<lootedFrom>[\w@]+)$/gm
 const chestLogRe = /^"(?<donatedAt>.*?)"\s"(?<donatedBy>[\w\d]*)"\s"(?<itemName>.*)"\s"(?<itemEnchant>\d*)"\s"\d*"\s"(?<amount>-?\d*)"$/gm
